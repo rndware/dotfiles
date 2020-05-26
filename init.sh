5d3cddc8c764d9b74b1d853aea30dea36da02203
Adam Smith <email@example.com>
2020-05-26 16:47:46 +0100

    Add settings file

--- init.sh
+++ init.sh
@@ -8,5 +8,10 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME';
 dotfiles pull;
 source ~/.zshrc;
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  cp settings.json ~/.config/Code/User;
+fi
+
+rm -rf settings.json;
 rm -rf README.md;
 rm -rf init.sh;
